b4cc250bbc6671320722a197e3664c95837e38d2
jlt0815 <jlt081509@gmail.com>
2023-10-04 00:48:27 -0400

    updated html

---
+++ Scripts/apps.js
@@ -0,0 +1,11 @@
+var app = (function () {
+    /* Properties */
+    var websiteName = "PLANE-CRASHES";
+    /* Methods */
+    return {
+        getWebsiteName: function () {
+            return websiteName;
+        }
+    }
+
+})();
